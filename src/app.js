b34785f110bb82d6599e0138eb2dd1294234e4cf
Ivan Smetanin <ivan.smetanin100@gmail.com>
2024-10-14 16:44:12 +0300

    v2

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const requestRoutes = require('./routes/requestRoutes');
+const cors = require('cors');
 const commentRoutes = require('./routes/commentRoutes');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 // Middleware для парсинга JSON
